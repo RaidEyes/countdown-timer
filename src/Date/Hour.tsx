d41640cdf621d6e9dd9ef0842888a888b3505533
RaidEyes <phamquanghung199713@gmail.com>
2023-12-19 13:36:00 +0700

    Add responsive to the design

--- src/Date/Hour.tsx
+++ src/Date/Hour.tsx
@@ -4,12 +4,12 @@ function Hour(props: { hour: number }) {
   return (
     <div>
       <div className="card">
-        <p className="text-[rgb(var(--primary-red))] relative before:absolute before:inset-0 before:h-3/6 before:bg-[rgb(var(--neutral-light-blue),0.3)]">
+        <p className="sm:text-6xl text-[rgb(var(--primary-red))] relative before:absolute before:inset-0 before:h-3/6 before:bg-[rgb(var(--neutral-light-blue),0.3)]">
           {props.hour}
         </p>
       </div>
       <h2 className="text-[0.5rem] tracking-[0.25rem] pt-4 text-white/40">
-        HOUR
+        HOURS
       </h2>
     </div>
   );
